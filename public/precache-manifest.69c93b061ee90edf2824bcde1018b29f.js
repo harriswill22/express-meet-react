5dd768e75b874dbfa1c0f725d28db90080bdc34b
Will Harris <Harriswill22@yahoo.com>
2018-12-11 14:23:42 -0500

    react app ready to deploy

---
+++ public/precache-manifest.69c93b061ee90edf2824bcde1018b29f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dda9895e382700bace42",
+    "url": "/static/js/main.dda9895e.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "dda9895e382700bace42",
+    "url": "/static/css/main.66e90278.chunk.css"
+  },
+  {
+    "revision": "053f6915415cd84145730a566b5b96a5",
+    "url": "/index.html"
+  }
+];
